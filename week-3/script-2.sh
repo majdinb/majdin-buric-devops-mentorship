e53dd5d67e72fcc27a24d4b5dde15d287b6eebe0
majdinb <majdin_b@hotmail.com>
2023-03-14 16:25:20 +0100

    adding scripts chapter 13

---
+++ week-3/script-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# testing the for variables after the looping
+
+for state in France England Spain Italy Portugal
+do
+   echo "The next state is $state"
+done
+echo "The last state we wisited was $state"
+state=Finland
+echo "Now we are visiting $state"
